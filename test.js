5c1ae678ed8f1f763c1a7421389fc78243b55512
Gyorgy Gabor <gyorgyg@gmail.com>
2017-02-27 13:35:06 +0100

    four destination works

--- test.js
+++ test.js
@@ -47,3 +47,28 @@ test('route gets "Y", "X", "Y" with "X => Y" rule', function (t) {
   t.deepEqual(holiday.route([{ dest: 'Y', rule: 'Y' }, { dest: 'X', rule: 'Y' }, { dest: 'Y', rule: 'Y' }]), ['Y', 'X']);
   t.end();
 });
+
+test('route gets "X", "X", "X"', function (t) {
+  t.deepEqual(holiday.route([{ dest: 'X', rule: 'X' }, { dest: 'X', rule: 'X' }, { dest: 'X', rule: 'X' }]), ['X']);
+  t.end();
+});
+
+test('route gets "X", "Y", "Z", "U" without rules', function (t) {
+  t.deepEqual(holiday.route([{ dest: 'X', rule: 'X' }, { dest: 'Y', rule: 'Y' }, { dest: 'Z', rule: 'Z' }, { dest: 'U', rule: 'U' }]), ['X', 'Y', 'Z', 'U']);
+  t.end();
+});
+
+test('route gets "X", "Y", "Z", "U" with "X => Y" rule', function (t) {
+  t.deepEqual(holiday.route([{ dest: 'X', rule: 'Y' }, { dest: 'Y', rule: 'Y' }, { dest: 'Z', rule: 'Z' }, { dest: 'U', rule: 'U' }]), ['Y', 'X', 'Z', 'U']);
+  t.end();
+});
+
+test('route gets "X", "Y", "Z", "U" with "X => Y, Y = U" rules', function (t) {
+  t.deepEqual(holiday.route([{ dest: 'X', rule: 'Y' }, { dest: 'Y', rule: 'U' }, { dest: 'Z', rule: 'Z' }, { dest: 'U', rule: 'U' }]), ['U', 'Y', 'X', 'Z']);
+  t.end();
+});
+
+test('route gets "X", "X", "Z", "U" with "X => Z" rule', function (t) {
+  t.deepEqual(holiday.route([{ dest: 'X', rule: 'Z' }, { dest: 'X', rule: 'Z' }, { dest: 'Z', rule: 'Z' }, { dest: 'U', rule: 'U' }]), ['Z', 'X', 'U']);
+  t.end();
+});
